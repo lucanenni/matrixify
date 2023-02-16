92aa705b0c1d8ff5a1df666829969ccb3044ee6f
lucanenni <luca.nenni@gmail.com>
2023-02-16 19:52:02 +0100

    Better performance: increased cell size, added willReadFrequently parameter

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
   const params = {
     glyphs: ['#','@','1','2','3','4','5','6','7','8','9','0'],
-    cell: 4,
+    cell: 6,
     tint: { r: 40, g: 231, b: 178, a: .6 }
   }
 
   const fontFamily = 'courier';  
   const typeCanvas = document.getElementById('canvas');
-  const typeContext = typeCanvas.getContext('2d');
+  const typeContext = typeCanvas.getContext('2d', { willReadFrequently: true });
   
   const magic = ({ video, context, width, height }) => {
     const cell = params.cell;
@@ -50,13 +50,13 @@
       
 	  const glyph = getGlyph(r);
   
-	  context.font = `${cell * 2}px ${fontFamily}`;
+	  context.font = `${cell}px ${fontFamily}`;
 	  
       context.fillStyle = `rgba(${params.tint.r}, ${params.tint.g}, ${params.tint.b}, ${params.tint.a}`;
 
       context.fontStyle
-      if (Math.random() < 0.1) {
-        context.font = `${cell * 3}px ${fontFamily}`;
+      if (Math.random() < 0.2) {
+        context.font = `${cell * 2}px ${fontFamily}`;
         context.fillStyle = `rgba(${params.tint.r+50}, ${params.tint.g+50}, ${params.tint.b+50}, ${params.tint.a+0.5}`;
       }
   
